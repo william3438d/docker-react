6b5a440cac13d3847e4d3b7ed7206716fd8e81e8
William Delahaye <william.delahaye@gmail.com>
2019-09-15 13:04:41 -0700

    final

---
+++ frontend/build/precache-manifest.0897642c632acab89be2cce31de9f8da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c3cef9c4e490ef9c1d3b0ad3622d02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21bbea429f69af080b4e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "20826a6408ce95dc8ca8",
+    "url": "/static/js/2.251f9854.chunk.js"
+  },
+  {
+    "revision": "21bbea429f69af080b4e",
+    "url": "/static/js/main.c1a6a71f.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
